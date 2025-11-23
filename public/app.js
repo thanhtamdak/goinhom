67bf94a1c600b3c0638a414e4c7f1396eb28b060
thanhtamdak <127088717+thanhtamdak@users.noreply.github.com>
2025-11-23 20:48:10 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,6 @@
-// Basic WebRTC logic placeholder
-const SIGNALING_URL = `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.hostname}:3000`;
 let localStream;
 const peers = {};
+let ws, roomId;
 
 const videoGrid = document.getElementById('videoGrid');
 const joinBtn = document.getElementById('joinBtn');
@@ -14,9 +13,10 @@ const shareScreenBtn = document.getElementById('shareScreen');
 const stopScreenBtn = document.getElementById('stopScreen');
 const leaveRoomBtn = document.getElementById('leaveRoom');
 
-let ws, roomId;
+// Use dynamic URL: works on Render/Railway
+const SIGNALING_URL = `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}`;
 
-// Helpers
+// Add video element
 function addVideo(stream, id, muted=false){
   const video = document.createElement('video');
   video.srcObject = stream;
@@ -37,14 +37,14 @@ async function initLocalStream(){
   addVideo(localStream, 'localVideo', true);
 }
 
-// Connect WebSocket
+// WebSocket connect
 function connectWS(){
   ws = new WebSocket(SIGNALING_URL);
   ws.onopen = ()=> statusDiv.innerText = 'Connected to signaling server';
   ws.onmessage = async (msg)=>{
     const data = JSON.parse(msg.data);
-    console.log('WS message', data);
-    // TODO: handle offer/answer/ice/new-peer/leave
+    // handle signaling messages here (offer/answer/ice/new-peer/leave)
+    console.log('WS message:', data);
   };
 }
 
@@ -54,27 +54,28 @@ joinBtn.onclick = async ()=>{
   roomId = roomInput.value;
   await initLocalStream();
   connectWS();
-  ws.onopen = ()=> {
-    ws.send(JSON.stringify({ type:'join', room:roomId }));
-  };
+  ws.onopen = ()=> ws.send(JSON.stringify({ type:'join', room:roomId }));
 };
 
-// Toggle controls
-toggleAudioBtn.onclick = ()=> {
-  localStream.getAudioTracks().forEach(t=> t.enabled = !t.enabled);
-};
-toggleVideoBtn.onclick = ()=> {
-  localStream.getVideoTracks().forEach(t=> t.enabled = !t.enabled);
-};
+// Controls
+toggleAudioBtn.onclick = ()=> localStream.getAudioTracks().forEach(t=> t.enabled=!t.enabled);
+toggleVideoBtn.onclick = ()=> localStream.getVideoTracks().forEach(t=> t.enabled=!t.enabled);
+
 shareScreenBtn.onclick = async ()=>{
   const screenStream = await navigator.mediaDevices.getDisplayMedia({ video:true });
   const screenTrack = screenStream.getVideoTracks()[0];
   const videoTrack = localStream.getVideoTracks()[0];
-  // Replace track logic placeholder
+  localStream.removeTrack(videoTrack);
+  localStream.addTrack(screenTrack);
+  addVideo(screenStream, 'screenVideo', true);
+  // TODO: replaceTrack for all peers
 };
+
 stopScreenBtn.onclick = ()=>{
-  // Restore camera logic placeholder
+  // TODO: replace back camera track
+  removeVideo('screenVideo');
 };
+
 leaveRoomBtn.onclick = ()=>{
   Object.keys(peers).forEach(id=>{/* close peer connections */});
   removeVideo('localVideo');
